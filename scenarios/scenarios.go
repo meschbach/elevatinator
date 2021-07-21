73d42817022799a99cf5cb02fb0dbb57b8784bc3
Mark Eschbach <mark.eschbach@truewhitespace.com>
2021-07-21 08:25:30 -0700

    scenarios/testing: Create test harness to apply a controller to a scenario

--- scenarios/scenarios.go
+++ scenarios/scenarios.go
@@ -3,6 +3,7 @@ package scenarios
 import (
 	"fmt"
 	"github.com/meschbach/elevatinator/simulator"
+	"testing"
 )
 
 type Scenario func(simulation *simulator.Simulation)simulator.Tick
@@ -23,6 +24,33 @@ func RunScenario(factory simulator.ControllerFunc, scenario Scenario)  {
 		fmt.Println("Event stream:")
 		for _, e := range stream.Events {
 			fmt.Printf("\t- %s\n", e.ToString())
+		}
+	}
+}
+
+func TestScenario(t *testing.T, factory simulator.ControllerFunc, scenario Scenario)  {
+	stream := simulator.NewEventLog()
+
+	simulation := simulator.NewSimulation()
+	simulation.AttachControllerListener(stream)
+	maxTicks := scenario(simulation)
+	simulation.AttachControllerFunc(factory)
+
+	tick := simulation.TickUpTo(maxTicks)
+	if simulation.ActorsCompletedObjectives() {
+		t.Logf("WIN!!! All actors completed objectives at tick %d", tick)
+	} else {
+		t.Errorf(":-( Some actors did not make it to their objectives @ tick %d", tick)
+		t.Logf("Event stream:")
+		for _, e := range stream.Events {
+			switch e.EventType {
+			case simulator.TickStart:
+				//do nothing
+			case simulator.TickDone:
+				t.Logf("-- Tick %d --", e.Timestamp)
+			default:
+				t.Logf("\t- %s\n",e.ToString())
+			}
 		}
 	}
 }
